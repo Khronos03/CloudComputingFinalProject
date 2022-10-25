c1d4a01b5a9ef4a079d6d21290a78d453f7d5b48
José Suaza <Jsuaza@intergrupo.com>
2022-10-24 21:19:17 -0500

    Se sube app a AWS S3

--- .vs/my-app/src/App.js
+++ .vs/my-app/src/App.js
@@ -1,10 +1,15 @@
+import logo from './logo.svg';
 import './App.css';
-import { Initial } from './Components/Initial';
 
 function App() {
   return (
     <div className="App">
-      <Initial />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Proyecto Final Daniel, Suaza.
+        </p>
+      </header>
     </div>
   );
 }
